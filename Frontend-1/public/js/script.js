abc6626f5db29ccfa1c6abc90da5c78c374202b3
Haise161 <nut.tie@dome.tu.ac.th>
2024-10-18 04:24:40 +0700

    Remove forgetpassword

--- Frontend-1/public/js/script.js
+++ Frontend-1/public/js/script.js
@@ -1,19 +1,3 @@
-function forgotPassword() {
-    const username = document.getElementById("username").value;
-    if (!username) {
-        document.getElementById("message").innerText = "Please enter your username to reset the password.";
-        return;
-    }
-
-    const newPassword = prompt("Enter your new password:");
-    if (newPassword) {
-        localStorage.setItem(username + "_password", newPassword);
-        document.getElementById("message").innerText = "Password has been reset successfully.";
-    } else {
-        document.getElementById("message").innerText = "Password reset cancelled.";
-    }
-}
-
 function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -21,22 +5,25 @@ function submitLogin() {
     fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Application-Key': "{{API_KEYS}}",
+            'Application-Key': "TU8253561ff033b374f429c6d9740343d47f313c7a4ce14e8d0e6be6e23821838a8394a9906070c144d71a45ba30b53c0d",
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-             "UserName": username,
-             "PassWord": password
+            "UserName": username,
+            "PassWord": password
         })
     })
-    .then(response => response.json())
-    .then(data => { if(data.type == "student"){
-        document.getElementById('message').innerHTML = `Welcome, ${data.displayname_th}<br> ${data.faculty}<br> ${data.department}`;
-    }else if(data.type == "employee"){
-        document.getElementById('message').innerHTML = `Welcome, ${data.displayname_th}<br> ${data.organization}<br> ${data.department}`;
-    }else {
-        document.getElementById('massage').innerHTML = "Invalid username or password. Please try again.";
-    }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.type == "student") {
+                document.getElementById('message').innerHTML = `Welcome, ${data.displayname_th}<br> ${data.faculty}<br> ${data.department}`;
+            } else if (data.type == "employee") {
+                document.getElementById('message').innerHTML = `Welcome, ${data.displayname_th}<br> ${data.organization}<br> ${data.department}`;
+            } else {
+                document.getElementById('message').innerHTML = "Invalid username or password. Please try again.";
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        });
 }
